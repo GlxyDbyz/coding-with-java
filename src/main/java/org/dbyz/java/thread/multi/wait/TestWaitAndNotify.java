1fbe4aed884ac2b323e27c22a3ccbc3ead94cdf7
GlxyDbyz <glxydbyz@gmail.com>
2015-11-06 21:12:17 +0800

    【新增&修改】线程等待例子修改，并增加等待时长的例子
    1.改成用main方法运行，修改部分注释和误解
    2.增加线程等待wait(time)的例子和解析

--- src/main/java/org/dbyz/java/thread/multi/wait/TestWaitAndNotify.java
+++ src/main/java/org/dbyz/java/thread/multi/wait/TestWaitAndNotify.java
@@ -1,43 +1,41 @@
 package org.dbyz.java.thread.multi.wait;
 
-import org.junit.Test;
 
 /**
- * 线程等待和唤醒(wait()会释放锁,等待再次唤醒,然后再去获取锁,然后继续执行)
+ * 线程等待和唤醒: wait()之后,当前线程会释放锁,进入等待状态,等待再次唤醒,然后再去竞争锁,然后继续执行;如果线程没有被唤醒,则线程一直处于等待状态,虚拟机不会退出
  *
- * @ClassName: TestWait
+ * @ClassName: TestWaitAndNotify
  * @author: 作者 E-mail <a href="mailto:845927437@qq.com">Dbyz</a>
  * @version: V1.0
  */
 public class TestWaitAndNotify {
 
-	@Test
-	public void testWait() throws InterruptedException {
+	public static void main(String[] args) throws InterruptedException {
 		new Thread(new MyThread(), "A1").start();
 		new Thread(new MyThread(), "A2").start();
-		
+
 		Thread.sleep(1000 * 2);// 使用 main 方法运行不需要此语句
-		
+
 		// 新建线程A1开始运行之后,遇到 lock.wait(); 马上进入了等待状态
-		// 线程A2开始运行,线程A2运行,并唤醒A1(A1等待获取锁),A2运行完了,释放锁,
+		// 线程A2开始运行,线程A2运行,并唤醒A1(A1等待获取锁),A2运行完成之后释放锁,
 		// A1获取锁之后继续从上次等待的地方开始运行,直到结束,然后主线程结束,程序退出
 
 		// 结果:
 		// Thread[A1,5,main] start
-		// Thread[A1,5,main] wait
+		// Thread[A1,5,main] wait                 // A1进入等待状态
 		// Thread[A2,5,main] start
 		// Thread[A2,5,main] run
-		// Thread[A2,5,main] notify others
+		// Thread[A2,5,main] notify one another   //lock(记住只有锁对象才能唤醒线程哦) 唤醒下一个线程(A1)
 		// Thread[A2,5,main] stop
 		// Thread[A1,5,main] run
-		// Thread[A1,5,main] notify others
+		// Thread[A1,5,main] notify one another   //lock 唤醒下一个线程(其实啥也没有了,当然他不会报错)
 		// Thread[A1,5,main] stop
 		// Thread[线程名称,线程优先级,线程组名称]
 	}
 
 	static class MyThread implements Runnable {
 		private final static String lock = "lock";
-		
+
 		private static int count = -1;
 
 		@Override
@@ -52,13 +50,14 @@ public class TestWaitAndNotify {
 					}
 
 					System.out.println(Thread.currentThread() + " run");
+					//....
 
-					if (MyThread.count % 2 == 1) {
-						System.out.println(Thread.currentThread()
-								+ " notify others");
-						lock.notify();// 唤醒一个线程(随机,此处只有线程 A2 )
-						// lock.notifyAll(); // 唤醒在 lock 上等待的所有线程
-					}
+					System.out.println(Thread.currentThread()
+							+ " notify one another");
+					lock.notify();// 唤醒一个线程(随机,此处只有线程 A2 )
+					// lock.notifyAll(); // 唤醒在 lock 上等待的所有线程
+					
+					
 					System.out.println(Thread.currentThread() + " stop");
 				} catch (InterruptedException e) {
 					e.printStackTrace();
---
+++ src/main/java/org/dbyz/java/thread/multi/wait/TestWaitAndNotify2.java
@@ -0,0 +1,59 @@
+package org.dbyz.java.thread.multi.wait;
+
+
+
+/**
+ * 线程等待和唤醒   : wait(time)之后,当前线程会释放锁,进入等待状态,等待再次唤醒,然后再去竞争锁,然后继续执行;如果线程没有被唤醒,则线程一直处于等待状态,虚拟机不会退出
+ *
+ * @ClassName: TestWaitAndNotify2
+ * @author: 作者 E-mail <a href="mailto:845927437@qq.com">Dbyz</a>
+ * @version: V1.0
+ */
+public class TestWaitAndNotify2 {
+	public static void main(String[] args) throws InterruptedException {
+		new Thread(new MyThread(), "B1").start();
+		new Thread(new MyThread(), "B2").start();
+		/**
+			Thread[B1,5,main] start
+			Thread[B1,5,main] wait(1000)   //A1进入等待状态,1000这个参数他其实并不知道,对他来说等同于调用了wait(),1000是lock需要知道的东西
+			Thread[B2,5,main] start
+			Thread[B2,5,main] run
+			Thread[B2,5,main] stop
+			Thread[B1,5,main] run          //在1000毫秒结束的时候,lock唤醒了在他上面等待的线程(有可能此时还有线程保持这锁,A1还是不能运行),此时B2早就释放了锁并结束了     (你也可以提前唤醒她,解除51行的注释即可)
+			Thread[B1,5,main] stop
+
+		 */
+	}
+
+	static class MyThread implements Runnable {
+		//自定义某个不变的变量
+		private final static Object lock = new Object();
+
+		private static int count = -1;
+
+		@Override
+		public void run() {
+			synchronized (lock) {
+				System.out.println(Thread.currentThread() + " start");
+				try {
+					MyThread.count++;
+					if (MyThread.count % 2 == 0) {
+						System.out.println(Thread.currentThread()
+								+ " wait(1000)");
+						lock.wait(1000);
+					}
+
+					System.out.println(Thread.currentThread() + " run");
+					Thread.sleep(10);
+
+					//lock.notify();
+
+					System.out.println(Thread.currentThread() + " stop");
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+
+}
