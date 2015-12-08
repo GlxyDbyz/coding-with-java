927920fffc7b1b267a0136cd37238c94f2f9dd9d
GlxyDbyz <glxydbyz@gmail.com>
2015-12-08 22:38:41 +0800

    【新增】JCU atuomic AtomicInteger简单例子

---
+++ src/main/java/org/dbyz/java/thread/JUC/atomic/AtomicIntegerTest.java
@@ -0,0 +1,37 @@
+package org.dbyz.java.thread.JUC.atomic;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * 支持原子操作的整型类
+ *
+ * @ClassName: AtomicIntegerTest
+ * @author: 作者 E-mail <a href="mailto:845927437@qq.com">Dbyz</a>
+ * @version: V1.0
+ */
+public class AtomicIntegerTest {
+	public static AtomicInteger count1 = new AtomicInteger(0);
+	public static Integer count2 = new Integer(0);
+
+	public static void main(String[] args) {
+		for (int i = 0; i < 10; i++) {
+			new Thread(new Runnable() {
+				public void run() {
+					for (int i = 0; i < 10000; i++) {
+						count1.incrementAndGet();
+						count2++;
+					}
+				}
+			}).start();
+		}
+
+		while (Thread.activeCount() > 1) {
+			Thread.yield();
+		}
+
+		System.out.println("count1:" + count1.get() + " count2:" + count2);
+	}
+	  //  count1:100000 count2:67694
+	
+	  //  结果很明显 AtomicInteger是线程安全的,而普通的Integer是线程不安全的有数据被覆盖
+}
---
+++ src/main/java/org/dbyz/java/thread/JUC/atomic/package-info.java
@@ -0,0 +1,11 @@
+package org.dbyz.java.thread.JUC.atomic;
+
+/**
+ * JUC autmic 包简单介绍
+ * 
+ * @author: 作者 E-mail <a href="mailto:845927437@qq.com">Dbyz</a> 
+ * @version: V1.0
+ */
+
+// java.util.concurrent.atomic 包内的工具类是支持原子操作的,操作他们无需再加另外的同步
+// 此处以AtomicInteger为例 其他的工具类使用方法类似
