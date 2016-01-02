537dba3ae838e6eed167d9b246ae834ac5d95c80
GlxyDbyz <glxydbyz@gmail.com>
2016-01-02 20:12:56 +0800

    【修改】简单修改

--- src/main/java/org/dbyz/frameworks/mongo/MongoCRUDTest.java
+++ src/main/java/org/dbyz/frameworks/mongo/MongoCRUDTest.java
@@ -1,5 +1,8 @@
 package org.dbyz.frameworks.mongo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.bson.Document;
 import org.bson.conversions.Bson;
 import org.junit.AfterClass;
@@ -8,7 +11,9 @@ import org.junit.Test;
 
 import com.mongodb.BasicDBObject;
 import com.mongodb.MongoClient;
+import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
 import com.mongodb.client.MongoDatabase;
 
 /**
@@ -32,6 +37,14 @@ public class MongoCRUDTest {
 	 */
 	private static MongoCollection<Document> books;
 
+	/**
+	 * 初始化资源
+	 * 
+	 * @Title: init
+	 * @param
+	 * @return: void
+	 * @since V1.0
+	 */
 	@BeforeClass
 	public static void init() {
 		client = new MongoClient("localhost", 27017);
@@ -39,16 +52,29 @@ public class MongoCRUDTest {
 		books = dataBase.getCollection("books");
 	}
 
+	/**
+	 * 释放资源
+	 * 
+	 * @Title: destory
+	 * @param
+	 * @return: void
+	 * @since V1.0
+	 */
 	@AfterClass
 	public static void destory() {
 		client.close();
 	}
 
 	/**
-	 * 增加
+	 * 添加1
+	 * 
+	 * @Title: test1Add1
+	 * @param
+	 * @return: void
+	 * @since V1.0
 	 */
 	@Test
-	public void test1Add() {
+	public void test1Add1() {
 		for (int i = 0; i < 100; i++) {
 			Document book = new Document();
 			book.append("name", "Java inaction" + i);
@@ -57,6 +83,26 @@ public class MongoCRUDTest {
 		}
 
 		System.out.println(books.count());
+	}
+
+	/**
+	 * 添加2
+	 * 
+	 * @Title: test1Add2
+	 * @param
+	 * @return: void
+	 * @since V1.0
+	 */
+	@Test
+	public void test1Add2() {
+		List<Document> documents = new ArrayList<Document>();
+		for (int i = 1000; i < 1100; i++) {
+			Document element = new Document();
+			element.append("name", "Java inaction" + i);
+			element.append("pricve", "$" + Math.random() * 100 * i);
+			documents.add(element);
+		}
+		books.insertMany(documents);
 	}
 
 	/**
@@ -69,8 +115,30 @@ public class MongoCRUDTest {
 	 */
 	@Test
 	public void test2Find() {
+		// 查找全部
+		System.out.println("find all ");
+		FindIterable<Document> iterable = books.find();
+		MongoCursor<Document> cursor = iterable.iterator();
+		while (cursor.hasNext()) {
+			System.out.println(cursor.next());
+		}
+
+		// 查找指定数据
+		System.out.println("find with condition");
 		Bson filter = new BasicDBObject("name", "Java inaction10");
-		System.out.println(books.find(filter).first());
+		cursor = books.find(filter).iterator();
+		while (cursor.hasNext()) {
+			System.out.println(cursor.next());
+		}
+		
+		Document element = new Document();
+		element.append("name", "Java inaction change");
+		element.append("pricve", "$" + Math.random() * 1000);
+		// 查找并删除
+		// books.findOneAndDelete(filter);
+		// 查找并跟新
+		// books.findOneAndReplace(filter, element);
+		
 	}
 
 	/**
@@ -91,7 +159,9 @@ public class MongoCRUDTest {
 		update.append("pricve", "$" + 1000);
 		// 查找并替换
 		books.findOneAndReplace(filter, update);
-
+		// 替换更新
+		// books.replaceOne(filter, update);
+		
 		System.out.println(books.find(filter).first());
 	}
 
@@ -109,7 +179,8 @@ public class MongoCRUDTest {
 		System.out.println(books.find(filter).first());
 
 		books.deleteOne(filter);
-
+		// books.deleteMany(filter);
+		
 		System.out.println(books.find(filter).first());
 	}
 
