8db69ee684a09aaedab5e7ad2b66274e2fa26fd6
GlxyDbyz <glxydby@gmail.com>
2016-08-24 16:10:16 +0800

    【修改】简单修改

--- src/main/java/org/dbyz/java/thread/JUC/FutureTest.java
+++ src/main/java/org/dbyz/java/thread/JUC/FutureTest.java
@@ -18,7 +18,7 @@ import org.junit.Test;
 public class FutureTest {
 	private static ExecutorService pool =  Executors.newCachedThreadPool();
 	@Test
-	public void test1() throws InterruptedException, ExecutionException{
+	public void test1_get() throws InterruptedException, ExecutionException{
 		System.out.println("1.Submit your task");
 		Future<String> future = pool.submit(new Callable<String>() {
 			@Override
@@ -32,5 +32,53 @@ public class FutureTest {
 		try {Thread.sleep(2000);} catch (Exception e) {}
 		
 		System.out.println("3.Get your result when you need it :"+future.get());
+	}
+	
+	@Test
+	public void test2_isDone() throws InterruptedException, ExecutionException{
+		System.out.println("1.Submit your task");
+		Future<String> future = pool.submit(new Callable<String>() {
+			@Override
+			public String call() throws Exception {
+				try {Thread.sleep(1);} catch (Exception e) {}
+				return "hello";
+			}
+		});
+		
+		while (!future.isDone()) {
+			System.out.println("Do other things");
+		}
+		
+		System.out.println("3.Task is Done, get your result:"+future.get());
+	}
+	
+	@Test
+	public void test3_cancle() throws InterruptedException, ExecutionException{
+		System.out.println("1.Submit your task");
+		Future<String> future = pool.submit(new Callable<String>() {
+			@Override
+			public String call() throws Exception {
+				try {Thread.sleep(2000);} catch (Exception e) {e.printStackTrace();}
+				return "hello";
+			}
+		});
+		
+		System.out.println("2.Do other things");
+		try {Thread.sleep(1000);} catch (Exception e) {}
+		
+		while (!future.isCancelled() && !future.isDone()) {
+			System.out.println("3.Wait tooooo long , cancel the task ...");
+//			future.cancel(true);
+			future.cancel(false);
+		}
+		
+		System.out.println("4.Can you get your result when task cancled ?");
+		try {
+			System.out.println(future.get());
+		} catch (Exception e) {
+			// CancellationException
+			e.printStackTrace();
+		}
+		
 	}
 }
