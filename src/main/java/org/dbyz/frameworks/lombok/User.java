edbc161b4bc52c95b9aef3bdf59b05116c335ce6
GlxyDbyz <glxydbyz@gmail.com>
2017-04-15 11:18:22 +0800

    [修改] 注释掉lombok代码

--- src/main/java/org/dbyz/frameworks/lombok/User.java
+++ src/main/java/org/dbyz/frameworks/lombok/User.java
@@ -7,16 +7,16 @@ import lombok.ToString;
 
 @Data
 public class User {
-    private final String name;
-    @Setter(AccessLevel.PACKAGE)
-    private int age;
-    private double score;
-    private String[] tags;
-
-    @ToString(includeFieldNames = true)
-    @Data(staticConstructor = "of")
-    public static class Exercise<T> {
-        private final String name;
-        private final T value;
-    }
+//    private final String name;
+//    @Setter(AccessLevel.PACKAGE)
+//    private int age;
+//    private double score;
+//    private String[] tags;
+//
+//    @ToString(includeFieldNames = true)
+//    @Data(staticConstructor = "of")
+//    public static class Exercise<T> {
+//        private final String name;
+//        private final T value;
+//    }
 }
